d5517bbda732680fbaa176f1441aed0fdaa8d3b1
Rama U <ramarangeswarareddy123@gmail.com>
2024-12-13 20:55:54 +0530

    custom http params and response type giving header sending

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
@@ -15,11 +15,22 @@ export class AppComponent implements OnInit {
 
     ngOnInit(): void {
         this.httpService
-            .get('https://jsonplaceholder.typicode.com/todos/1')
+            .get('https://jsonplaceholder.typicode.com/todos/1', {
+                responseType: 'json',
+            })
             .subscribe({
                 next: (data) => {
                     console.log(data);
                 },
             });
+        const params = new HttpParams();
+        const headers = new HttpHeaders();
+        this.httpService
+            .get('https://jsonplaceholder.typicode.com/comments', {
+                responseType: 'json',
+                params: params.set('postId', 3),
+                headers: headers.append('debug-leven','normal')
+            })
+            .subscribe((res) => console.log(res));
     }
 }
