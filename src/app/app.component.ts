0652e582045ab6d3fcb772fc28b3fe2b3e803449
Rama U <ramarangeswarareddy123@gmail.com>
2024-12-13 22:34:29 +0530

    asyync pipe type arg for http

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,36 +1,23 @@
 import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { UserService } from './user.service';
+import { AsyncPipe } from '@angular/common';
+import { Observable } from 'rxjs';
 
 @Component({
     selector: 'app-root',
     standalone: true,
-    imports: [RouterOutlet],
+    imports: [RouterOutlet, AsyncPipe],
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss',
 })
 export class AppComponent implements OnInit {
     title = 'ng-2024-12';
-    httpService: HttpClient = inject(HttpClient);
+    userServce: UserService = inject(UserService);
+    user$: Observable<{title:string}> = new Observable();
 
     ngOnInit(): void {
-        this.httpService
-            .get('https://jsonplaceholder.typicode.com/todos/1', {
-                responseType: 'json',
-            })
-            .subscribe({
-                next: (data) => {
-                    console.log(data);
-                },
-            });
-        const params = new HttpParams();
-        const headers = new HttpHeaders();
-        this.httpService
-            .get('https://jsonplaceholder.typicode.com/comments', {
-                responseType: 'json',
-                params: params.set('postId', 3),
-                headers: headers.append('debug-leven','normal')
-            })
-            .subscribe((res) => console.log(res));
+        this.user$ = this.userServce.getUser();
     }
 }
