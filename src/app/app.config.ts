54ed31006305392f1ab43d59b6106d805c37363b
Rama U <ramarangeswarareddy123@gmail.com>
2024-12-13 07:41:20 +0530

    advand moder fetch AI instead of default XMMLHttpReqeust which is old

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,13 +2,13 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
 import { logginInterceptor } from './loggin.interceptor';
 
 export const appConfig: ApplicationConfig = {
     providers: [
         provideZoneChangeDetection({ eventCoalescing: true }),
         provideRouter(routes),
-        provideHttpClient(withInterceptors([logginInterceptor])),
+        provideHttpClient(withInterceptors([logginInterceptor]),withFetch()),
     ],
 };
