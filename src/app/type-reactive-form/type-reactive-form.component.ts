f90982c5208216812b3a390095afd56638efe31e
Rama U <ramarangeswarareddy123@gmail.com>
2024-12-15 14:51:08 +0530

    optional controll we can add or remove to the from the fromgroup

--- src/app/type-reactive-form/type-reactive-form.component.ts
+++ src/app/type-reactive-form/type-reactive-form.component.ts
@@ -1,17 +1,23 @@
 import { Component } from '@angular/core';
-import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+interface LoginForm {
+    email: FormControl<string>;
+    password?: FormControl<string>;
+}
 
 @Component({
-  selector: 'app-type-reactive-form',
-  standalone: true,
-  imports: [ReactiveFormsModule],
-  templateUrl: './type-reactive-form.component.html',
-  styleUrl: './type-reactive-form.component.scss'
+    selector: 'app-type-reactive-form',
+    standalone: true,
+    imports: [ReactiveFormsModule],
+    templateUrl: './type-reactive-form.component.html',
+    styleUrl: './type-reactive-form.component.scss',
 })
 export class TypeReactiveFormComponent {
-  email = new FormControl<string|null>(null);
-  resetForm() { 
-    this.email.setValue(null); // throws error infer type null so
-    
+    login = new FormGroup<LoginForm>({
+        email: new FormControl('', { nonNullable: true }),
+        password: new FormControl('', { nonNullable: true }),
+    });
+  removeContol() { 
+    this.login.removeControl('password')
   }
 }
