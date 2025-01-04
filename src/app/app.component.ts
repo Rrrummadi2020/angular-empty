8c689d6af645753f3566543bf55e413c797b99e9
Rama U <ramarangeswarareddy123@gmail.com>
2025-01-04 09:47:11 +0530

    useClass

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,3 @@
-import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { UserService } from './user.service';
@@ -6,20 +5,22 @@ import { AsyncPipe } from '@angular/common';
 import { Observable } from 'rxjs';
 import { FormControl, ReactiveFormsModule } from '@angular/forms';
 import { ReactiveFormComponent } from './reactive-form/reactive-form.component';
-import { TypeReactiveFormComponent } from "./type-reactive-form/type-reactive-form.component";
-import { TempDrivenComponent } from "./temp-driven/temp-driven.component";
+import { TypeReactiveFormComponent } from './type-reactive-form/type-reactive-form.component';
+import { TempDrivenComponent } from './temp-driven/temp-driven.component';
+import { AuthService } from './auth.service';
 
 @Component({
     selector: 'app-root',
     standalone: true,
     imports: [
-    RouterOutlet,
-    AsyncPipe,
-    ReactiveFormsModule,
-    ReactiveFormComponent,
-    TypeReactiveFormComponent,
-    TempDrivenComponent
-],
+        RouterOutlet,
+        AsyncPipe,
+        ReactiveFormsModule,
+        ReactiveFormComponent,
+        TypeReactiveFormComponent,
+        TempDrivenComponent,
+    ],
+    providers: [{ provide: AuthService, useClass: AuthService }],
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss',
 })
@@ -28,6 +29,7 @@ export class AppComponent implements OnInit {
     user$: Observable<{ title: string }> = new Observable();
 
     userServce: UserService = inject(UserService);
+    authService: AuthService = inject(AuthService);
 
     ngOnInit(): void {
         this.user$ = this.userServce.getUser();
